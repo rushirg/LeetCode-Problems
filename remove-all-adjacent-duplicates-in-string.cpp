5f648b783b241db5b823447f625afa5c698b1a00
rushirg <gaidhani.rushikesh@gmail.com>
2019-11-01 23:37:10 -0400

    Added solution

---
+++ remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,16 @@
+/*
+ *Solution for problem 1047
+ *https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+ */
+class Solution {
+public:
+    string removeDuplicates(string S) {
+        string res = "";
+        for (char& c : S)
+            if (res.size() && c == res.back())
+                res.pop_back();
+            else
+                res.push_back(c);
+        return res;
+    }
+};
