ac26a6f27ee249e77aa25be5866ad6ad86d43862
rushirg <gaidhani.rushikesh@gmail.com>
2019-11-10 22:30:14 -0500

    Added solution of 217

--- contains-duplicate.cpp
+++ contains-duplicate.cpp
@@ -2,7 +2,20 @@
  *Solution of problem 217
  *https://leetcode.com/problems/contains-duplicate/
  */
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
 
+        sort(nums.begin(), nums.end());
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i] == nums[i-1])
+                return true;
+        }
+        return false;
+    }
+};
+
+/*
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
@@ -15,3 +28,5 @@ public:
         return false;
     }
 };
+=======
+*/
