20f24d290e9aeb79936c37f3b78478ccf9899945
rushirg <gaidhani.rushikesh@gmail.com>
2019-11-11 16:39:14 -0500

    Added solution of 389

--- find-the-difference.cpp
+++ find-the-difference.cpp
@@ -16,3 +16,16 @@ public:
         return NULL;
     }
 };
+
+/*
+ * class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char ans = 0;
+        for(char c: s) ans ^= c;
+        for(char c: t) ans ^= c;
+        return ans;
+    }
+};
+*
+* /
