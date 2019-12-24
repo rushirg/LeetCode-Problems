9b2fa09ae9149e686751efb54326f4aa11c2b685
rushirg <gaidhani.rushikesh@gmail.com>
2019-12-24 16:31:15 -0500

    Restructure files
    - moving files to solution directory

--- big-countries.sql
+++
@@ -1,6 +0,0 @@
-/*
-Solution for problem 595
-https://leetcode.com/problems/big-countries/
-*/
-# Write your MySQL query statement below
-SELECT name, population, area FROM World Where population > 25000000 OR area > 3000000
