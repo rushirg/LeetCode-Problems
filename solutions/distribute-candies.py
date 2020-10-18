544585383d6987abbd9e7ec523b7614a59e15c35
rushirg <gaidhani.rushikesh@gmail.com>
2020-10-18 16:27:57 -0400

    add solution of 575
    575. Distribute Candies
    - https://leetcode.com/problems/distribute-candies/

---
+++ solutions/distribute-candies.py
@@ -0,0 +1,32 @@
+"""
+575. Distribute Candies
+https://leetcode.com/problems/distribute-candies/
+"""
+
+
+### Solution 1
+
+from collections import Counter
+class Solution:
+    def distributeCandies(self, candies: List[int]) -> int:
+        candiesCounter = Counter(candies)
+        count = len(candies) // 2
+        result = 0
+
+        while count != 0:
+            for k, v in candiesCounter.items():
+                if count == 0:
+                    break
+                if v is 0:
+                    continue
+                candiesCounter[k] -= 1
+                result += 1
+                count -= 1
+            return result
+
+
+### Solution 2
+
+class Solution:
+    def distributeCandies(self, candies: List[int]) -> int:
+        return min(len(candies) // 2, len(set(candies)))
