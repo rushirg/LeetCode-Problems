feee1b3553be05d8b3d1cf1bfb66e95be1d53b24
rushirg <gaidhani.rushikesh@gmail.com>
2021-03-05 14:36:17 -0500

    update 414
    414. Third Maximum Number
    - https://leetcode.com/problems/third-maximum-number/
    - https://leetcode.com/explore/featured/card/fun-with-arrays/523/conclusion/3231/

--- solutions/third-maximum-number.py
+++ solutions/third-maximum-number.py
@@ -1,9 +1,10 @@
 """
 414. Third Maximum Number
-https://leetcode.com/problems/third-maximum-number/
+- https://leetcode.com/problems/third-maximum-number/
+- https://leetcode.com/explore/featured/card/fun-with-arrays/523/conclusion/3231/
 """
 
-
+# Method 1
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
         nums = list(set(nums))
@@ -11,3 +12,23 @@ class Solution:
         if len(nums) < 3:
             return nums[0]
         return nums[2]
+
+
+# Method 2
+# O(N)
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        firstMax = secondMax = thirdMax = None
+        for x in nums:
+            if x == firstMax or x == secondMax or x == thirdMax:
+                continue
+            if firstMax == None or x > firstMax:
+                thirdMax = secondMax
+                secondMax = firstMax
+                firstMax = x
+            elif secondMax == None or x > secondMax:
+                thirdMax = secondMax
+                secondMax = x
+            elif thirdMax == None or x > thirdMax:
+                thirdMax = x
+        return firstMax if thirdMax == None else thirdMax
