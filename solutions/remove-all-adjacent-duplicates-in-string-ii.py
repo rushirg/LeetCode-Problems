4638aa6584e15a1b6de364b8f41d2feffa3ccf63
rushirg <gaidhani.rushikesh@gmail.com>
2021-04-16 10:26:01 -0400

    add solution of 1209
    1209. Remove All Adjacent Duplicates in String II
    - https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii/
    - https://leetcode.com/explore/featured/card/april-leetcoding-challenge-2021/595/week-3-april-15th-april-21st/3710/

---
+++ solutions/remove-all-adjacent-duplicates-in-string-ii.py
@@ -0,0 +1,25 @@
+"""
+1209. Remove All Adjacent Duplicates in String II
+- https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii/
+- https://leetcode.com/explore/featured/card/april-leetcoding-challenge-2021/595/week-3-april-15th-april-21st/3710/
+"""
+
+# Method 1
+# Using stack to store the character and count
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        myStack = [(s[0], 1)]
+        count = 1
+        for i in range(1, len(s)):
+            if myStack and myStack[-1][0] == s[i]:
+                myStack.append((s[i], myStack[-1][1] + 1))
+            else:
+                count = 1
+                myStack.append((s[i], count))
+            if myStack[-1][1] == k:
+                myStack = myStack[:len(myStack) - k]
+        result = ""
+        for ch in myStack:
+            result += ch[0]
+        return result
+
