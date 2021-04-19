e51103b5649845be16fc7a75c6e091ee93987878
rushirg <gaidhani.rushikesh@gmail.com>
2021-04-19 16:29:10 -0400

    update solution of 2
    2. Add Two Numbers
    https://leetcode.com/problems/add-two-numbers

--- solutions/add-two-numbers.py
+++ solutions/add-two-numbers.py
@@ -2,6 +2,55 @@
 2. Add Two Numbers
 https://leetcode.com/problems/add-two-numbers/
 """
+
+
+# Method 1
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        carry = 0
+        result = n = ListNode(0)
+        while l1 or l2 or carry:
+            v1 = v2 = 0
+            if l1:
+                v1 = l1.val
+                l1 = l1.next
+            if l2:
+                v2 = l2.val
+                l2 = l2.next
+            # newValue = v1 + v2 + carry
+            # carry = newValue // 10
+            # newValue = newValue % 10
+            carry, newValue = divmod(v1 + v2 + carry, 10)
+            n.next  = ListNode(newValue)
+            n = n.next
+        return result.next
+                
+
+# Method 2
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        carry = 0
+        result = n = ListNode(0)
+        while l1 or l2 or carry:
+            v1 = v2 = 0
+            if l1:
+                v1 = l1.val
+                l1 = l1.next
+            if l2:
+                v2 = l2.val
+                l2 = l2.next
+            # newValue = v1 + v2 + carry
+            # carry = newValue // 10
+            # newValue = newValue % 10
+            carry, newValue = divmod(v1 + v2 + carry, 10)
+            n.next  = ListNode(newValue)
+            n = n.next
+        return result.next
+
+
+
+# Method 2
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
