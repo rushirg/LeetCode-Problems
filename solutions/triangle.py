a37581287b3715982445827623252148d979a51f
rushirg <gaidhani.rushikesh@gmail.com>
2021-04-21 10:32:40 -0400

    add solution of 120
    120. Triangle
    - https://leetcode.com/problems/triangle/
    - https://leetcode.com/explore/featured/card/april-leetcoding-challenge-2021/595/week-3-april-15th-april-21st/3715/

---
+++ solutions/triangle.py
@@ -0,0 +1,25 @@
+"""
+120. Triangle
+- https://leetcode.com/problems/triangle/
+- https://leetcode.com/explore/featured/card/april-leetcoding-challenge-2021/595/week-3-april-15th-april-21st/3715/
+"""
+
+# Method 1
+# Time: O(N)
+
+class Solution(object):
+    def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+        for i in range(1, len(triangle)):
+            for j in range(len(triangle[i])):
+                if j == 0:
+                    triangle[i][j] =  triangle[i - 1][j] + triangle[i][j]
+                elif j == len(triangle[i]) - 1:
+                    triangle[i][j] = triangle[i-1][j-1] + triangle[i][j]
+                else:
+                    triangle[i][j] = min(triangle[i - 1][j - 1] + triangle[i][j], triangle[i - 1][j] + triangle[i][j])
+            print(triangle[i])
+        return min(triangle[-1])
